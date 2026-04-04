98f742dcc729ebbb1feac58f1def39b079bcbf2f
Parth <parthhkdigiverse@gmail.com>
2026-04-04 15:09:29 +0530

    Fix: Completely removed domain restrictions (ALLOWED_HOSTS='*') from all settings files

--- aisetu_erp/aisetu_erp/aisetu_erp/settings.py
+++ aisetu_erp/aisetu_erp/aisetu_erp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-54*!$s_g01(7+l-h522603rapr+uk*c$#yp9s^btl*dw=#&^r*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # Project-wide host permit
 
 
 # Application definition
